ded16e955c6d441f7819d1479a4e37c0b7cd59ea
Ethan Conville <ethan@conville.com>
2025-04-27 12:35:47 -0500

    Some movement with DIce

--- scenes/game_board.gd
+++ scenes/game_board.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 @onready var player_1 : Sprite2D = $Player
 @export var game_spaces : Array[Node]
-var place : int = 1
+var place : int = 0
 var number_of_spaces : int
 @onready var dice := $Dice
 @onready var timer := $Timer
@@ -14,9 +14,15 @@ func _ready() -> void:
 func _on_dice_dice_has_rolled(roll: Variant) -> void:
 	print(roll)
 	while(roll != 0):
+		await(move(place))
+		place += 1
+		roll -= 1
+		#piece has stopped moving
+		if roll == 0:
+			pass
+
+func move(place) -> void: 
 		var tween = create_tween()
 		tween.tween_property(player_1, "position", game_spaces[place].position, 1)
 		timer.start()
 		await timer.timeout
-		place += 1
-		roll -= 1
